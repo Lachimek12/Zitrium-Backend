92bb61d4afa63b28d1277a78874f3bae82a5c181
Lachimek12 <140963714+Lachimek12@users.noreply.github.com>
2024-07-31 11:14:35 +0200

    Delete dist directory

--- dist/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const userController_1 = require("../controllers/userController");
-const router = require("express").Router();
-router.post('/register', userController_1.registerUser);
-router.post('/register/resend-verify', userController_1.resendVerificationEmail);
-router.get('/verify-email', userController_1.verifyEmail);
-module.exports = router;
-//# sourceMappingURL=user.routes.js.map
